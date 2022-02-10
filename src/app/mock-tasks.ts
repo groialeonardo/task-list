f3d8a4a760b200783310d5a958dc206dbc0242a1
groialeonardo <51366574+groialeonardo@users.noreply.github.com>
2022-02-10 14:58:21 -0300

    Se agregan archivos de tareas mock

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,39 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    id:1,
+    text:"Terminar primer modulo de Angular",
+    day: "Agosto 5 a las 17:00 hs",
+    reminder: true
+  },
+
+  {
+    id:2,
+    text:"Terminar de mandar correos",
+    day: "Agosto 15 a las 18:00 hs",
+    reminder: false
+  },
+
+  {
+    id:3,
+    text:"Tomarme unos mates",
+    day: "Agosto 21 a las 11:00 hs",
+    reminder: true
+  },
+
+  {
+    id:4,
+    text:"Tomarme unos mates",
+    day: "Agosto 21 a las 11:00 hs",
+    reminder: true
+  },
+
+  {
+    id: 5,
+    text:"Ir a practicar Basket",
+    day: "Agosto 21 a las 21:00 hs",
+    reminder: false
+  },
+
+]
