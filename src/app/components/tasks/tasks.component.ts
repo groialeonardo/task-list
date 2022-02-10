c2f204cd150336dd5672ad971f1fdb437562a1f2
groialeonardo <51366574+groialeonardo@users.noreply.github.com>
2022-02-10 16:57:20 -0300

    Se crea servicio sincronico para tareas

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+
+
 
 @Component({
   selector: 'app-tasks',
@@ -10,11 +12,15 @@ import { TASKS } from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks:Task[] = TASKS;
+  tasks:Task[] = [];
 
-  constructor() { }
+  constructor(
+    private taskService:TaskService
+  ) { }
 
   ngOnInit(): void {
+
+    this.tasks=this.taskService.getTasks()
   }
 
 }
