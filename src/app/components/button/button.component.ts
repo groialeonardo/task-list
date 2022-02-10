66b515adcb36d64c3dd4bca84113550a13d3db90
groialeonardo <51366574+groialeonardo@users.noreply.github.com>
2022-02-10 14:32:31 -0300

    Se agrega logica de btn click

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -9,6 +9,7 @@ export class ButtonComponent implements OnInit {
 
   @Input() text:string= "";
   @Input() color:string= "";
+  @Output() onBbtnClick = new EventEmitter();
 
   constructor() { }
 
@@ -17,7 +18,8 @@ export class ButtonComponent implements OnInit {
 
   onClick(){
 
-    console.log("Clicckk")
+    this.onBbtnClick.emit();
+
   }
 
 }
